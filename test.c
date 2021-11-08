e8115c46b016e8af46e33cd1e0633fdeaf0302d8
mayashley <54293844+mayashley@users.noreply.github.com>
2021-11-08 10:41:50 -0500

    getting first number, opperator, and 2nd number for the calc.

--- test.c
+++ test.c
@@ -1,8 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-int age;
-printf("Enter age\n");
-scanf("age is %d", age);
-return 0;
+int main()
+{
+    double user_num1;
+    double user_num2;
+    char operator;
+
+    printf("Enter your first number: ");
+    scanf("%lf", &user_num1);
+    printf("Enter your operator:");
+    // when getting a character in c  with scanf you always need a space befor the % sign or it will error out
+    scanf(" %c", &operator);
+    printf("Enter your second number");
+    scanf("%1f, &user_num2: ");
+    return 0;
 }
